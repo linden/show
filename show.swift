e549be7b217ec73f916f9c668e9ff7c0bc8cef12
Linden <70739041+linden@users.noreply.github.com>
2021-11-16 01:52:18 -0800

    Initial commit

---
+++ show.swift
@@ -0,0 +1,43 @@
+import AppKit
+import WebKit
+
+let app = NSApplication.shared
+
+class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
+	let window = NSWindow.init(contentRect: NSRect(x: 0, y: 0, width: 750, height: 600), styleMask: [ 
+		NSWindow.StyleMask.titled, 
+		NSWindow.StyleMask.closable,
+		NSWindow.StyleMask.resizable
+	], backing: NSWindow.BackingStoreType.buffered, defer: false)
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+		if (CommandLine.arguments.count < 2) {
+			print("URL argument required, example: show https://github.com/linden/show\r\n")
+			app.terminate(self)
+		}
+	
+		let url = URL(string: CommandLine.arguments[1])!
+		
+       	window.makeKeyAndOrderFront(nil)
+	   	window.center()
+		
+		window.delegate = self
+		window.title = "show"
+	   	
+	   	let webview = WKWebView(frame: window.contentView!.frame)
+	   	let request = URLRequest(url: url)
+		       	
+	   	window.contentView?.addSubview(webview)
+	   	webview.load(request)
+    }
+	
+	func windowWillClose(_ notification: Notification) {
+		app.terminate(self)
+	}
+}
+
+let delegate = AppDelegate()
+
+app.delegate = delegate
+
+app.run()
